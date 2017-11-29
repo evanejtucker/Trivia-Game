390187c2946cf008ad588f272b2cd7606c10fba7
Evan Tucker <eej.tucker@gmail.com>
2017-11-29 15:35:42 -0700

    more gamescripting

--- public/assets/javascript/game.js
+++ public/assets/javascript/game.js
@@ -7,6 +7,8 @@ $(document).ready(function(){
     var selectedQuestion;
     let startingTime = 20;
     var intervalID;
+    var wins = 0;
+    var loses = 0;
 
 // Functions
 // ----------------------------------------------------------------------------------------------------
@@ -18,22 +20,40 @@ $(document).ready(function(){
         } return arr;
     }
 
+    // randomizes an array
+    var shuffleArray = (array) => {
+        var shuffled = array.sort(function(a, b){return 0.5 - Math.random()});
+        return shuffled;
+    }
+
+    // remove spaces from word, used to create values from multiword strings
+    var removeSpaces = (word) => {
+        var newWord = word.replace(/\s/g, '');
+        return newWord;
+    }
+
     // set question to be displayed
     var setSelectedQuestion = () => {
         selectedQuestion = questionOptions[Math.floor(Math.random() * questionOptions.length)];
-        console.log(selectedQuestion);
+        displayQuestion(selectedQuestion);
+    }
+
+    // update the timer in the dom
+    var updateTimer = () => {
+        $("#timer").text(startingTime);
     }
 
     // start the clock
     var startTimer = () => {
+
+        startingTime = 20;
         intervalID = setInterval(runningTime, 1000);
     }
 
     // reduce the time, and updata the dom
     var runningTime = () => {
         startingTime--;
-        console.log(startingTime);
-
+        updateTimer();
         endTimer();
     }
 
@@ -42,9 +62,46 @@ $(document).ready(function(){
 
         if (startingTime === 0) {
             clearTimeout(intervalID);
-            startingTime = 20;
-            console.log("time is out");
+            updateTimer();
         }
+    }
+
+    // displays questions and answers
+    var displayQuestion = (question) => {
+
+        clearDisplayedQuestion();
+        startTimer();
+
+        $("#question").text(question.question);
+
+        shuffleArray(question.answers);
+
+        for (var i=0; i<question.answers.length; i++) {
+
+            let ans = question.answers[i];
+            let value = removeSpaces(ans);
+            $("#answers").append("<button class='answer' value=" + value + "><h4>" + ans + "</h4></button>");
+        }
+
+        userGuess(question);
+    }
+
+    var userGuess = (question) => {
+        $(".answer").on("click", function() {
+            console.log(this.value);
+            if (this.value === removeSpaces(question.correctAnswer)) {
+                console.log("correct");
+                wins++;
+            } else {
+                console.log("incorrect");
+                loses++;
+            }
+        });
+    }
+
+    var clearDisplayedQuestion = () => {
+        $("#question").empty();
+        $("#answers").empty();
     }
 
 
@@ -59,7 +116,7 @@ $(document).ready(function(){
 
         console.log(questionOptions);
         setSelectedQuestion();
-        startTimer();
+
     });
 
 

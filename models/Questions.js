68aebd6e22b09a3dec707b730ee659478b249fa9
Evan Tucker <eej.tucker@gmail.com>
2017-11-29 17:33:42 -0700

    added questions

--- models/Questions.js
+++ models/Questions.js
@@ -14,6 +14,7 @@ var QuestionsSchema = new Schema({
   },
   answers: [{
     type: String || Number,
+    unique: false,
     required: true
   }],
   correctAnswer: {
--- server.js
+++ server.js
@@ -33,7 +33,16 @@ db.once('open', function() {
     var q1 = new Questions({question: "Who was the First President of the United States?", answers: ["George Washington", "John Adams", "Thomas Jefferson", "Alexander Hamilton"], correctAnswer: "George Washington"});
     var q2 = new Questions({question: "When was the constitution written?", answers: ["1776", "1807", "1787", "1782"], correctAnswer: "1787"});
     // saveToDB(q2);
-
+    var q3 = new Questions({question: "Which one of these was NOT one of the thirteen original colonies?", answers: ["Maine","Deleware","New Hampshire","Georgia"], correctAnswer: "Maine"});
+    var q4 = new Questions({question: "Who is the Cheif Justice of the United States now?", answers: ["John Roberts","Rex Tillerson","James Mattis","Jeff Sessions"], correctAnswer: "John Roberts"});
+    var q5 = new Questions({question: "What territory did the Unites States buy from France in 1803?", answers: ["Louisiana","Northwest","California","Mississippi"], correctAnswer: "Louisiana"});
+    var q6 = new Questions({question: "Before he was president, Eissenhower was a general.  What war did he fight in?", answers: ["World War II","Korean War","World War I","Vietnam War"], correctAnswer: "World War II"});
+    var q7 = new Questions({question: "How many amendements does the constitution have?", answers: ["Twenty Seven (27)","Ten (10)","Thirteen (13)","Twenty Three (23)"], correctAnswer: "Twenty Seven (27)"});
+    var q8 = new Questions({question: "How many Justices are on the Supreme Court?", answers: ["Nine (9)","Thirteen (13)","Three (3)","Seven (7)"], correctAnswer: "Nine (9)"});
+    var q9 = new Questions({question: "Who wrote the Decleration of Independence?", answers: ["Thomas Jefferson","Alexander Hamilton","John Hancock","John Adams"], correctAnswer: "Thomas Jefferson"});
+    var q10 = new Questions({question: "If both the President, and the Vice President can no longer serve, who becomes President?", answers: ["The Speaker of the House","Secretary of State","Secretary of Defense","Attorney General"], correctAnswer: "The Speaker of the House"});
+    var q11 = new Questions({question: "The House of Representatives has how many voting members?", answers: ["Four Hundred Thirty-Five (435)","One Hundred (100)","Four Hundred Fifteen (415)","Two Hundred Thirty Seven (237)"], correctAnswer: "Four Hundred Thirty-Five (435)"});
+    saveToDB(q8);
 });
 
 app.get('/', function (req, res) {
